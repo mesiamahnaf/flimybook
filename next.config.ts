69ab44a44591ac8dcfdc235691ea87d896429db7
Siam Ahnaf <siamahnaf198@gmail.com>
2025-08-31 20:19:52 +0600

    dsgdgf

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     remotePatterns: [{
       protocol: "https",
       hostname: "sn1odfv42uxzov8w.public.blob.vercel-storage.com"
+    }, {
+      protocol: "https",
+      hostname: "image.tmdb.org"
     }]
   }
 };
